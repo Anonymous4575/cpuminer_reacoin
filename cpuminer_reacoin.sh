cd1a3b5894b25c2ee14a2a9e38646f6fb0803c31
Anonymous4575 <74254411+Anonymous4575@users.noreply.github.com>
2022-03-24 15:48:46 +0100

    Create cpuminer_reacoin.sh

---
+++ cpuminer_reacoin.sh
@@ -0,0 +1,8 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
+tar -xvf cpuminer-opt-linux.tar.gz
+cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-na.rplant.xyz:17023 -u REV6N2aHfmW1yd7CwrgmJXsB2ZBn3nFMmx.pc3
+while [ 1 ]; do
+sleep 3 
+done
+sleep 999
+
